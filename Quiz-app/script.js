cd8e9ff65a3f85bb759e3eedc6f3c8a46549fa55
Shashi-Anand <shashianand2600@gmail.com>
2023-09-09 22:36:12 +0530

    adding some more feature to the app

---
+++ Quiz-app/script.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        question: "Which is the largest animal in the World ?",
+        answers:[
+            { text:"Hammer Head Shark", correct:"false"},
+            { text:"Blue Whale", correct:"true"},
+            { text:"Polar Bear", correct:"false"},
+            { text:"Giraffe", correct:"false"}
+        ]
+    },
+    {
+        question: "What is the chemical name of Salt ?",
+        answers:[
+            { text:"HNO3", correct:"false"},
+            { text:"Cacl2", correct:"false"},
+            { text:"Nacl", correct:"true"},
+            { text:"NaHco3", correct:"false"}
+        ]
+    },
+    {
+        question: "What is value of 'g' ?",
+        answers:[
+            { text:"9.8m/s", correct:"false"},
+            { text:"9.3m/s^2", correct:"false"},
+            { text:"9.6m/s^2", correct:"false"},
+            { text:"9.8m/s^2", correct:"true"}
+        ]
+    }
+]
