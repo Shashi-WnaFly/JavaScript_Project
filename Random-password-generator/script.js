16e64d55c927cbbb69d6c34f0d9a7327c669b222
Shashi Anand <shashianand2600@gmail.com>
2024-01-22 22:30:35 +0530

    almost javascript done but copy function is not working now cause execCommand deprecated

---
+++ Random-password-generator/script.js
@@ -0,0 +1,31 @@
+let password = document.querySelector('#password');
+let copi = document.querySelector('#copy');
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const num = "0123456789";
+const symbol = "~@#$%^&*()_+/=-{}][";
+const allChars = upperCase+lowerCase+num+symbol;
+
+const len = 12;
+
+function createPassword(){
+
+    let pass = "";
+    pass += upperCase[Math.floor(Math.random()*26)];
+    pass += lowerCase[Math.floor(Math.random()*26)];
+    pass += num[Math.floor(Math.random()*10)];
+    pass += symbol[Math.floor(Math.random()*19)];
+
+    while(len > pass.length){
+        pass += allChars[Math.floor(Math.random()*81)];
+    }
+    password.value = pass;
+    pass = null;
+}
+
+copi.addEventListener( () => {
+    password.select();
+    // navigator.clipboard.writeText(password.value);
+    // document.execCommand("copy");
+})
