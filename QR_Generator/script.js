d3b9bb4ecd05679b1db27b7a74d1198cca53d22f
Shashi Anand <shashianand2600@gmail.com>
2024-02-02 10:35:26 +0530

    java script has almost done

--- QR_Generator/script.js
+++ QR_Generator/script.js
@@ -1,3 +1,11 @@
 let input_txt = document.querySelector('#input_txt');
 let btn = document.querySelector('#btn');
+let img = document.querySelector('#qr_code');
 
+btn.addEventListener('click', () => {
+    if(input_txt.value === "") alert("Please Enter some value!");
+    else{
+        img.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + input_txt.value;
+        
+    }
+})
