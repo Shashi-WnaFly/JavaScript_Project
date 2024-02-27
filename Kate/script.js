504301f066f6252328567a370d188676061b5218
Shashi Anand <shashianand2600@gmail.com>
2024-02-27 16:27:48 +0530

    project almost complete

--- Kate/script.js
+++ Kate/script.js
@@ -1,12 +1,29 @@
 let wb = document.querySelector('#wb');
 
+if(localStorage.getItem("theme") == null){
+    localStorage.setItem("theme", "light");
+}
+
+let stat = localStorage.getItem("theme");
+
+if(stat == "light"){
+    wb.src = './images/moon.png';
+    document.body.classList.remove("dark-theme");
+}
+else if(stat == "dark"){
+    wb.src = './images/sun.png';
+    document.body.classList.add("dark-theme");
+}
+
 wb.addEventListener('click', ()=>{
+    document.body.classList.toggle("dark-theme");
+
     if(document.body.classList.contains('dark-theme')){
-        wb.src = './images/moon.png';
+        wb.src = './images/sun.png';
+        localStorage.setItem("theme", "dark");
     }
     else{
-        wb.src = './images/sun.png';
-
+        wb.src = './images/moon.png';
+        localStorage.setItem("theme", "light");
     }
-    document.body.classList.toggle("dark-theme");
 })
