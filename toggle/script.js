315e39e83293499cb2b4285b334055a54d17b232
Shashi Anand <shashianand2600@gmail.com>
2024-02-28 19:58:59 +0530

    toggle btn complete

--- toggle/script.js
+++ toggle/script.js
@@ -3,6 +3,7 @@ let body = document.querySelector('body');
 let round = document.querySelector('.roundBtn');
 
 btn.addEventListener('click', ()=>{
-    // body.style.background = '#212121';
+    body.classList.toggle('back');
     round.classList.toggle('right');
+    btn.classList.toggle('btn_m');
 })
