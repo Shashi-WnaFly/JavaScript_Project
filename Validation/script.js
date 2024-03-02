c310435a4989d35995dc7bec2ff5bff232e8d6ac
Shashi Anand <shashianand2600@gmail.com>
2024-03-02 22:48:48 +0530

    almost done and submit function is remaining

--- Validation/script.js
+++ Validation/script.js
@@ -4,16 +4,80 @@ let email_err = document.querySelector('#email-err');
 let message_err = document.querySelector('#message-err');
 let submit_err = document.querySelector('#submit-err');
 
-function nameError(){
+function validateName(){
     let name = document.getElementById('contact_name').value;
     if(name.length == 0){
         name_err.innerHTML = 'Name is required';
+        name_err.style.color = 'red'
         return false;
     }
     if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
         name_err.innerHTML = 'Write full name';
+        name_err.style.color = 'red'
         return false;
     }
     name_err.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    name_err.style.color = 'seagreen';
     return true;
+}
+
+function validatePhone(){
+    let phone = document.getElementById('contact_phone').value;
+    if(phone.length == 0){
+        phone_err.innerHTML = 'phone no. is required';
+        phone_err.style.color = 'red'
+        return false;
+    }
+    else if(phone.length !== 10){
+        phone_err.innerHTML = 'phone should be 10 digits';
+        phone_err.style.color = 'red'
+        return false;
+    }
+    else if(phone.match(/^[0-9]{10}$/)){
+        phone_err.innerHTML = 'phone no. is required';
+        phone_err.style.color = 'red'
+        return false;
+    }
+    phone_err.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    phone_err.style.color = 'seagreen';
+    return true;
+}
+
+function validateEmail(){
+    let email = document.getElementById('contact_email').value;
+    if(email.length == 0){
+        email_err.innerHTML = 'Email is required';
+        email_err.style.color = 'red';
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        email_err.innerHTML = 'invalid email';
+        email_err.style.color = 'red';
+        return false;
+    }
+    email_err.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    email_err.style.color = 'seagreen';
+    return true;
+}
+
+function validateMsg(){
+    let msg = document.querySelector('#contact_msg').value;
+    let c = 30-msg.length;
+    if(msg.length < 30){
+        message_err.innerHTML = c+` character are remaining`;
+        message_err.style.color = 'red';
+        return false;
+    }
+    message_err.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    message_err.style.color = 'seagreen';
+    return true;
+}
+
+function validateForm(){
+    let subBtn = document.getElementById('subBtn');
+    if(validateMsg() && validateEmail() && validatePhone() && validateName()){
+        alert('Your form successfully submitted.')
+    }
+    submit_err.innerHTML = 'Please fix some error';
+    submit_err.style.color = 'red';
 }
