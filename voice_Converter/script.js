8d19ea1440bdc14493a9c1d99659077e5aaf4feb
Shashi Anand <shashianand2600@gmail.com>
2024-03-11 18:26:33 +0530

    project complete

--- voice_Converter/script.js
+++ voice_Converter/script.js
@@ -1,5 +1,20 @@
 let speech = new SpeechSynthesisUtterance();
 
+let voices = [];
+
+let select_list = document.querySelector('select');
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => (select_list.options[i] = new Option(voice.name, i)));
+}
+
+select_list.addEventListener('change', ()=>{
+    speech.voice = voices[select_list.value];
+})
+
 document.querySelector('button').addEventListener('click', ()=>{
     speech.text = document.querySelector('textarea').value;
     window.speechSynthesis.speak(speech);
