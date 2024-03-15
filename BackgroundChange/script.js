5ea9a29c51bb0befd71aad392da4321158cf32d6
Shashi Anand <shashianand2600@gmail.com>
2024-03-15 13:32:41 +0530

    all function works fine

--- BackgroundChange/script.js
+++ BackgroundChange/script.js
@@ -1,10 +1,14 @@
 let org = document.querySelector('.img_org');
 let img_box = document.querySelector('.img_box');
+let origin = document.querySelector('#origin');
+let line = document.getElementById('hor-line');
 
 let leftSpace = img_box.offsetLeft;
+origin.style.width = img_box.offsetWidth+"px";
 
 img_box.onmousemove = function (e){
     let boxWidth = (e.pageX - leftSpace) + "px";
 
     org.style.width = boxWidth;
+    line.style.left = boxWidth;
 }
